2264385e5c83d496c6266d35568d13c3a5748370
sajidulhaque007 <sajidulhaque007@gmail.com>
2024-08-19 12:19:24 +0600

    ..

--- server.js
+++ server.js
@@ -13,8 +13,7 @@ app.use(passport.initialize());
 
 mongoose
   .connect(process.env.MONGO_URI, {
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true,
+
   })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
